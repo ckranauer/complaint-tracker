940fdadbe0278e3aca325662d95a267a27dec7c0
Krana <krana.csaba@gmail.com>
2021-12-08 13:08:12 +0100

    spring initilaizr generated prj added to the folder

---
+++ claim-db/src/main/java/com/compalinttracker/claimdb/ClaimDbApplication.java
@@ -0,0 +1,13 @@
+package com.compalinttracker.claimdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimDbApplication.class, args);
+	}
+
+}
---
+++ claim-db/src/test/java/com/compalinttracker/claimdb/ClaimDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compalinttracker.claimdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
