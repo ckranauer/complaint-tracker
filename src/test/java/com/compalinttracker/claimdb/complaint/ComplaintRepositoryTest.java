8924473e5fc98d528a37edd80df3052af0e46e67
Krana <krana.csaba@gmail.com>
2022-03-30 13:20:01 +0200

    ComplaintRepositoryTest created

---
+++ src/test/java/com/compalinttracker/claimdb/complaint/ComplaintRepositoryTest.java
@@ -0,0 +1,64 @@
+package com.compalinttracker.claimdb.complaint;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComplaintRepositoryTest {
+
+    @Test
+    void itShouldFindAllComplaintAnalysis() {
+        // Given
+        // When
+        // Then
+
+    }
+
+    @Test
+    void itShouldFindComplaintAnalysisById() {
+        // Given
+        // When
+        // Then
+
+    }
+
+    @Test
+    void itShouldFindComplaintById() {
+        // Given
+        // When
+        // Then
+
+    }
+
+    @Test
+    void itShouldFindComplaintBySerNo() {
+        // Given
+        // When
+        // Then
+
+    }
+
+    @Test
+    void itShouldFindComplaintByQmsNo() {
+        // Given
+        // When
+        // Then
+
+    }
+
+    @Test
+    void itShouldDeleteComplaintById() {
+        // Given
+        // When
+        // Then
+
+    }
+
+    @Test
+    void itShouldRemoveResponsible() {
+        // Given
+        // When
+        // Then
+
+    }
+}
